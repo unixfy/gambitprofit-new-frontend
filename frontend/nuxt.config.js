9a6cd0e0b81e08fe0f6643b2eaa99ffe85bfc415
Alex Wang <admin@unixfy.me>
2021-01-01 15:44:03 -0500

    Fix some SEO problems

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -11,11 +11,11 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - GambitProfit',
-    title: 'frontend',
+    title: 'GambitProfit',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'The easy way to profit from Gambit Rewards!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
