c0dde6fd5f7b518a1260e94a47b752a39cace9ce
Alex <alex@unixfy.net>
2022-08-01 23:07:22 +0000

    Set up Tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import '@fontsource/inter';
+	import '../app.css';
+	import { darkmode } from '../stores';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (browser) {
+			// Enable darkmode automatically if it's saved in localstorage
+			if ($darkmode) {
+				document.documentElement.classList.add('dark');
+			} else {
+				document.documentElement.classList.remove('dark');
+			}
+		}
+	});
+</script>
+
+<div class="min-h-screen"><slot /></div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
