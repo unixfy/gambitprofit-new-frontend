6908b5a3537a632053af47c4f13c9ac5bd716e5a
Alex <alex@unixfy.net>
2022-08-04 23:53:30 +0000

    Bootstrap index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import MatchDisplay from '@components/MatchDisplay.svelte'
+</script>
+
+<div class="hero homepage-bg">
+	<div class="hero-overlay bg-opacity-40 dark:bg-opacity-60" />
+	<div class="hero-content py-12 px-8 min-h-[33vh] text-center text-neutral-content">
+		<div>
+			<h1 class="mb-5 text-3xl sm:text-4xl font-bold">Welcome to 💰 GambitProfit</h1>
+			<p class="mb-5 text-lg sm:text-xl">The data portal for Gambit Rewards plays.</p>
+		</div>
+	</div>
+</div>
+
+<div class="ct max-w-screen-xl px-2 py-6 sm:py-8 flex flex-col space-y-6 sm:space-y-8">
+	<div class="alert alert-info text-white">
+		<div>
+			<p>
+				<b>Welcome to the new version of GambitProfit.</b> With a new API server and fresh interface written from scratch, you'll have a streamlined and more reliable experience.
+				Note that some features, like notifications, remain under development. Stay tuned to see them roll out.
+			</p>
+		</div>
+	</div>
+</div>
+
+<MatchDisplay/>
