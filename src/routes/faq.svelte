2970117fa634d664a7bb1089a4e9e7f91942af1c
Alex <alex@unixfy.net>
2022-08-04 22:26:22 +0000

    Compose a FAQ page

---
+++ src/routes/faq.svelte
@@ -0,0 +1,23 @@
+<script>
+	import PageHeader from '../components/PageHeader.svelte';
+	export let faqs;
+</script>
+
+<PageHeader
+	title="Frequently Asked Questions"
+	subtitle="Commonly asked questions about GambitProfit"
+/>
+
+<div class="flex flex-col space-y-4 max-w-screen-sm mx-auto py-8 px-4">
+	{#each faqs as faq}
+		<div
+			class="collapse collapse-plus border border-gray-300 dark:border-slate-500 bg-gray-100 dark:bg-slate-800 rounded-box"
+		>
+			<input type="checkbox" />
+			<div class="collapse-title text-xl font-medium">{faq.q}</div>
+			<div class="collapse-content">
+				<p>{@html faq.a}</p>
+			</div>
+		</div>
+	{/each}
+</div>
