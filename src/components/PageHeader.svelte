63dc3a9463111cfebf99c577baf2c5fcb4dd3e38
Alex <alex@unixfy.net>
2022-08-04 22:37:31 +0000

    Improve page header responsiveness

--- src/components/PageHeader.svelte
+++ src/components/PageHeader.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="bg-gray-200 dark:bg-slate-700 dark:text-white p-12 sm:p-16 flex flex-col space-y-6 text-center">
-	<h1 class="font-bold capitalize text-4xl">{title}</h1>
+	<h1 class="font-bold capitalize text-3xl sm:text-4xl">{title}</h1>
 	{#if subtitle}
-		<h2 class="font-thin text-2xl">{subtitle}</h2>
+		<h2 class="font-thin text-xl sm:text-2xl">{subtitle}</h2>
 	{/if}
 </div>
