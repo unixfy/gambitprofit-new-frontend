94a1202f8c693b0ebff813c56d05fb4ef029ad8d
Alex <alex@unixfy.net>
2022-08-07 23:36:21 +0000

    Work around dayjs esm issue

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,12 @@ const config = {
 		alias: {
 			'@components': path.resolve('./src/components')
 		}
-	}
+	},
+	ssr: {
+        // We have to add this workaround because of #4504 https://github.com/sveltejs/kit/issues/4504
+        // In the future DayJS 2.0 will not require this workaround
+        noExternal: ['dayjs']
+    }
 };
 
 export default config;
