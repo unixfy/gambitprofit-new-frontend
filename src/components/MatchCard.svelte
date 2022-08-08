64d1ff74c3877691f9a658b9628bb7d1be85bf08
Alex <alex@unixfy.net>
2022-08-08 00:14:32 +0000

    Mock out UI for homepage

--- src/components/MatchCard.svelte
+++ src/components/MatchCard.svelte
@@ -1,10 +1,24 @@
+<script>
+	import dayjs from "dayjs/esm";
+	import relativeTime from "dayjs/esm/plugin/relativeTime";
+	export let match;
+
+	dayjs.extend(relativeTime)
+
+	let active_bettype_index = 0;
+
+	function setActiveBettypeIndex(i) {
+		active_bettype_index = i;
+	}
+</script>
+
 <div class="card bg-base-100 shadow-lg border">
 	<div class="card-body px-4 py-2">
 		<div class="flex">
-			<p class="card-title my-auto text-ellipsis">Unixfy v. Team</p>
+			<p class="card-title my-auto text-ellipsis">{match.name}</p>
 			<div class="tooltip tooltip-left" data-tip="Open on GambitRewards">
 				<a
-					href="https://app.gambitrewards.com"
+					href={match.url}
 					rel="noreferer"
 					target="_blank"
 					class="ml-auto btn btn-circle  btn-ghost"
@@ -12,10 +26,7 @@
 				</a>
 			</div>
 			<div class="tooltip tooltip-left" data-tip="More info about this match">
-				<a
-					href="/match/asdfasfasfd"
-					class="ml-2 btn btn-circle btn-ghost"
-				>
+				<a href="/match/{match.id}" class="ml-2 btn btn-circle btn-ghost">
 					<i class="fa-solid fa-file-lines" />
 				</a>
 			</div>
@@ -23,20 +34,35 @@
 
 		<div class="flex flex-col space-y-2">
 			<div class="tabs tabs-boxed bg-base-100 p-0 mb-2">
-				<a class="tab tab-active ">Pick the Winner</a>
-				<a class="tab ">Spread</a>
+				{#each match.bettypes as bettype, i}
+					<!-- a really janky way to implement tabs for bet types -->
+					<a
+						class="tab"
+						class:tab-active={active_bettype_index == i}
+						on:click={() => setActiveBettypeIndex(i)}>{bettype.name}</a
+					>
+				{/each}
 			</div>
 
 			<div class="stats stats-horizontal p-1">
 				<div class="stat p-0 place-items-center">
 					<div class="stat-title text-sm">No-Risk Profit</div>
-					<div class="stat-value text-lg">5%</div>
+					<div class="stat-value text-lg">
+						{match.bettypes[active_bettype_index].no_risk.profit_sb_percentage}%
+					</div>
 				</div>
 				<div class="stat p-0 place-items-center">
-					<div class="stat-title text-sm">Good Bet Types</div>
+					<div class="stat-title text-sm">Bet Types</div>
 					<div class="stat-value text-lg">
-						<span class="badge badge-outline badge-error">High Risk</span>
-						<span class="badge badge-outline badge-success">No Risk</span>
+						{#if match.bettypes[active_bettype_index].no_risk.possible}
+							<span class="badge badge-outline badge-success">No Risk</span>
+						{/if}
+						{#if match.bettypes[active_bettype_index].med_risk.possible}
+							<span class="badge badge-outline badge-warning">Med Risk</span>
+						{/if}
+						{#if match.bettypes[active_bettype_index].high_risk.possible}
+							<span class="badge badge-outline badge-error">High Risk</span>
+						{/if}
 					</div>
 				</div>
 			</div>
@@ -45,22 +71,23 @@
 				<p class="text-sm opacity-60 font-light">
 					<i class="fa-solid fa-info-circle" /> no-risk bets
 				</p>
-				<p><b>Unixfy (1.02):</b> Bet <span class="badge">102</span> tokens</p>
-				<p><b>Team (2.54):</b> Bet <span class="badge">103</span> tokens</p>
+				{#each match.bettypes[active_bettype_index].no_risk.bet as {team, amount}}
+				<p><b>{team}:</b> Bet <span class="badge">{amount}</span> tokens</p>
+				{/each}
 			</div>
 
 			<div>
 				<p class="text-sm opacity-60 font-light"><i class="fa-solid fa-clock" /> cutoff</p>
-				<p>Today at 08:00pm</p>
+				<p>{dayjs(match.cutoff_datetime).fromNow()}</p>
 			</div>
 
 			<div>
 				<p class="text-sm opacity-60 font-light"><i class="fa-solid fa-shuffle" /> sport</p>
-				<p>Baseball</p>
+				<p>{match.sport}</p>
 			</div>
 		</div>
 		<div class="card-actions justify-end">
-			<p class="opacity-60 italic text-xs">Last updated today at 08:00pm EDT</p>
+			<p class="opacity-60 italic text-xs">Last updated {dayjs(match.last_updated).fromNow()}</p>
 		</div>
 	</div>
 </div>
--- src/components/MatchDisplay.svelte
+++ src/components/MatchDisplay.svelte
@@ -1,14 +1,37 @@
 <script>
 	import MatchCard from './MatchCard.svelte';
+	import { fireRequest } from '$lib/fireRequest';
+	import { onMount } from 'svelte';
 
 	let loading = true;
+	let data = {};
 	let token_amount = 1000;
 	let search_term = '';
 	let sort_by = 'datetime';
+	// whether to show non no-risk recommended - inverted
 	let show_all = false;
 	let sport = '';
+	// whether to show plays that passed the cutoff - inverted
 	let historical = false;
 	let historical_date = '';
+
+	async function fetchData() {
+		loading = true;
+
+		let req = await fireRequest('/matches/', {
+			no_risk_recommended: true,
+			cutoff_passed: true
+		});
+
+		let req_data = await req.json();
+
+		data = req_data;
+		loading = false;
+	}
+
+	onMount(async () => {
+		fetchData();
+	});
 </script>
 
 <div class="drawer drawer-mobile h-auto pb-8">
@@ -23,43 +46,48 @@
 						class="btn btn-primary btn-square drawer-button lg:hidden"
 						aria-label="Show filters"><i class="fa-solid fa-sliders" /></label
 					>
-					<button class="btn btn-primary btn-square" aria-label="Refresh data" class:loading>
+					<button class="btn btn-primary btn-square" aria-label="Refresh data" class:loading on:click="{fetchData}">
 						{#if !loading}
 							<i class="fa-solid fa-refresh" />
 						{/if}
 					</button>
 				</div>
 			</div>
-			<div class="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-				{#each Array(16) as item}
-					<MatchCard />
+			<div class="grid md:grid-cols-2 grid-cols-1 gap-4">
+				{#if loading}
+					<progress class="progress col-span-3 progress-primary" />
 				{:else}
-					<div class="alert alert-warning col-span-4">
-						<div>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="stroke-current flex-shrink-0 h-6 w-6"
-								fill="none"
-								viewBox="0 0 24 24"
-								><path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-								/></svg
-							>
-						<span>Oops, looks like there are no plays at this time. Try looking at our historical data.</span>
+					{#each data.results as item}
+						<MatchCard match={item} />
+					{:else}
+						<div class="alert alert-warning col-span-4">
+							<div>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									class="stroke-current flex-shrink-0 h-6 w-6"
+									fill="none"
+									viewBox="0 0 24 24"
+									><path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+									/></svg
+								>
+								<span
+									>Oops, looks like there are no plays at this time. Try looking at our historical
+									data.</span
+								>
+							</div>
 						</div>
-					</div>
-				{/each}
+					{/each}
+				{/if}
 			</div>
 		</div>
 	</div>
 	<div class="drawer-side shadow-lg">
 		<label for="settings-drawer" class="drawer-overlay" />
-		<div
-			class="menu px-2 py-2 w-80 bg-base-300 flex flex-col space-y-4"
-		>
+		<div class="menu px-2 py-2 w-80 bg-base-300 flex flex-col space-y-4">
 			<label
 				for="settings-drawer"
 				class="btn btn-primary drawer-button lg:hidden"
@@ -207,7 +235,7 @@
 				</div>
 			</div>
 
-			<button class="btn btn-secondary btn-block">Apply settings</button>
+			<button class="btn btn-secondary btn-block" class:loading>Apply settings</button>
 		</div>
 	</div>
 </div>
