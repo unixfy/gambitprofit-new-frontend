06f2f5222ee064e3a20ccf70aeb69f090bd2b148
Alex <alex@unixfy.net>
2022-08-08 03:53:17 +0000

    Implement some basic meta tags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,13 +11,49 @@
 		if (browser) {
 			// Enable darkmode automatically if it's saved in localstorage
 			if ($darkmode) {
-                document.documentElement.dataset.theme = 'dark';
+				document.documentElement.dataset.theme = 'dark';
 			} else {
-                document.documentElement.dataset.theme = 'light';
+				document.documentElement.dataset.theme = 'light';
 			}
 		}
 	});
 </script>
+
+<svelte:head>
+	<!-- Primary Meta Tags -->
+	<title> | GambitProfit</title>
+	<meta name="title" content="| GambitProfit" />
+	<meta
+		name="description"
+		content="The data portal for Gambit Rewards plays. Easily browse, sort, search through, and share plays!"
+	/>
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://gambitprofit.com" />
+	<meta property="og:title" content="GambitProfit" />
+	<meta
+		property="og:description"
+		content="The data portal for Gambit Rewards plays. Easily browse, sort, search through, and share plays!"
+	/>
+	<meta
+		property="og:image"
+		content="/advert_screenshot.png"
+	/>
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://gambitprofit.com" />
+	<meta property="twitter:title" content="GambitProfit" />
+	<meta
+		property="twitter:description"
+		content="The data portal for Gambit Rewards plays. Easily browse, sort, search through, and share plays!"
+	/>
+	<meta
+		property="twitter:image"
+		content="/advert_screenshot.png"
+	/>
+</svelte:head>
 
 <Navbar />
 
