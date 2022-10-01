cb7f0fd622eafe93661558228757e5d3a71cb953
Alex Wang <unixfy@users.noreply.github.com>
2022-10-01 14:49:51 -0400

    Reflect discount change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,6 +19,9 @@
 	<div class="alert alert-info text-white">
 		<div>
 			<p>
+				<b>On 10/01/2022 at 12:15am Pacific, the 10% discount changed to 5%.</b> All new plays will reflect this discount, and all plays whose cutoffs had not passed on 10/01/2022 at 11:20am Pacific were retroactively updated to reflect the new discount.
+			</p>
+			<p>
 				<b>The <a href="/donate" target="_blank">Donate page</a> is now live!</b> The next features will be persistent settings and notifications...
 			</p>
 		</div>
