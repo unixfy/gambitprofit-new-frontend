20460f0130fdeedafa96e3d26330ca8553d0ef0b
unixfy <alex@unixfy.net>
2023-06-07 13:50:50 -0400

    Add 404 handler for /match/[uuid]

--- src/routes/match/[uuid]/+page.server.js
+++ src/routes/match/[uuid]/+page.server.js
@@ -1,8 +1,15 @@
 import {fireRequest} from "$lib/fireRequest.js";
+import {error} from "@sveltejs/kit";
 
 export async function load({params, url}) {
-    let req = await fireRequest(`/matches/${params.uuid}`, {'tokens': url.searchParams.get('tokens') ? url.searchParams.get('tokens') : '1000'})
+    const req = await fireRequest(`/matches/${params.uuid}`, {'tokens': url.searchParams.get('tokens') ? url.searchParams.get('tokens') : '1000'})
+
+    // Handle 404s from the API endpoint
+    if (req.status === 404) {
+        throw error(404, 'Match not found')
+    }
+
     return {
-        match: req.ok && (await req.json())
+        match: (await req.json())
     }
 }
