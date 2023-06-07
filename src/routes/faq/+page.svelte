6f1c9cc66ae257c5b13e97a7a1aeee423ecae9a7
unixfy <alex@unixfy.net>
2023-06-07 13:43:48 -0400

    Use data prop to load FAQs

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,8 +1,6 @@
 <script>
-	throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
-
 	import PageHeader from '$lib/PageHeader.svelte';
-	export let faqs;
+	export let data;
 
 	import {title} from '../../stores';
 	
@@ -15,7 +13,7 @@
 />
 
 <div class="flex flex-col space-y-4 max-w-screen-sm mx-auto py-8 px-4">
-	{#each faqs as faq}
+	{#each data.faqs as faq}
 		<div
 			class="collapse collapse-plus border bg-base-100 rounded-box"
 		>
