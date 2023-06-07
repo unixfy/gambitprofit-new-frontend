f16f16d36e096bdb4b55b493a7982e0dbb3f4397
unixfy <alex@unixfy.net>
2023-06-07 14:42:20 -0400

    Use SvelteKit native fetch() instead of window.fetch() in /match/[uuid]

--- src/routes/match/[uuid]/+page.js
+++ src/routes/match/[uuid]/+page.js
@@ -1,8 +1,17 @@
-import {fireRequest} from "$lib/fireRequest.js";
 import {error} from "@sveltejs/kit";
 
-export async function load({params, url}) {
-    const req = await fireRequest(`/matches/${params.uuid}`, {'tokens': url.searchParams.get('tokens') ? url.searchParams.get('tokens') : '1000'})
+export async function load({fetch, params, url}) {
+    const req = await fetch(
+       import.meta.env.VITE_API_ENDPOINT + `/matches/${params.uuid}?` + new URLSearchParams({
+            'tokens': url.searchParams.get('tokens') ? url.searchParams.get('tokens') : '1000'
+        }) ,
+        {
+            method: "GET",
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+    )
 
     const match = (await req.json())
     // Handle 404s from the API endpoint
