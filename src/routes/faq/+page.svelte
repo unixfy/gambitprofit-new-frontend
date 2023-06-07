749a27effb3c993c7c913ca3e3aa1bd7493e9d27
unixfy <alex@unixfy.net>
2023-06-07 14:44:07 -0400

    Use accordion instead of collapse on /faq

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -11,9 +11,9 @@
 <div class="flex flex-col space-y-4 max-w-screen-sm mx-auto py-8 px-4">
 	{#each data.faqs as faq}
 		<div
-			class="collapse collapse-plus border bg-base-100 rounded-box"
+			class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box"
 		>
-			<input type="checkbox" />
+			<input type="radio" name="faq-accordion"/>
 			<div class="collapse-title text-xl font-medium">{faq.q}</div>
 			<div class="collapse-content">
 				<p>{@html faq.a}</p>
