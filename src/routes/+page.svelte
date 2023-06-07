51db1156c3ecd9c7d9e13d3d30bca1b777fd5fd8
unixfy <alex@unixfy.net>
2023-06-07 14:22:26 -0400

    Remove wrapper divs inside alerts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,30 @@
 <script>
-	import MatchDisplay from '$lib/MatchDisplay.svelte';
+    import MatchDisplay from '$lib/MatchDisplay.svelte';
 </script>
 
 <div class="hero homepage-bg">
-	<div class="hero-overlay bg-opacity-40" />
-	<div class="hero-content py-12 px-8 min-h-[33vh] text-center text-neutral-content">
-		<div class="text-white">
-			<h1 class="mb-5 text-3xl sm:text-4xl font-bold">Welcome to 💰 GambitProfit</h1>
-			<p class="mb-5 text-lg sm:text-xl">The data portal for Gambit Rewards plays.</p>
-		</div>
-	</div>
+    <div class="hero-overlay bg-opacity-40"/>
+    <div class="hero-content py-12 px-8 min-h-[33vh] text-center text-neutral-content">
+        <div class="text-white">
+            <h1 class="mb-5 text-3xl sm:text-4xl font-bold">Welcome to 💰 GambitProfit</h1>
+            <p class="mb-5 text-lg sm:text-xl">The data portal for Gambit Rewards plays.</p>
+        </div>
+    </div>
 </div>
 
 <div class="ct max-w-screen-xl px-2 py-6 sm:py-8 flex flex-col space-y-2 sm:space-y-4">
-	<a href="/donate">
-		<div class="alert alert-success text-white">
-			<div class="flex-col">
-				<p><b>Help keep GambitProfit running and support development.</b> Click me to donate.</p>
-			</div>
-		</div>
-	</a>
-	<div class="alert alert-info text-white">
-		<div class="flex-col">
-			<p>
-				<b>New!</b> Your settings (token amount, sort, search, etc.) will now persist in your browser.
-			</p>
-		</div>
-	</div>
+    <a href="/donate">
+        <div class="alert alert-success text-white">
+            <p><b>Help keep GambitProfit running and support development.</b> Click me to donate.</p>
+        </div>
+    </a>
+    <div class="alert alert-info text-white">
+        <p>
+            <b>New!</b> Your settings (token amount, sort, search, etc.) will now persist in your browser.
+        </p>
+    </div>
 </div>
 
 <div class="max-w-[1920px] mx-auto">
-	<MatchDisplay />
+    <MatchDisplay/>
 </div>
